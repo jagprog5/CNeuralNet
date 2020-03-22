031c4cc1bfb8b64e804a5ac4556d1ccc34ee7a2b
John Giorshev <jgiorshev@localhost.localdomain>
2020-03-22 02:02:12 -0400

    Fully functional! Added softmax activation and cross entropy error.

--- main.c
+++ main.c
@@ -6,110 +6,105 @@
 #include "FFNN.h"
 #include "MNISTRead.h"
 
+void MNISTVisualSGD(struct FFNN* ffnn,
+                    float** inputs, 
+                    float** outputs, 
+                    int trainingSetSize, 
+                    float learningRate,
+                    int width,
+                    int height);
+
 int main() {
-    int layers[3] = {2, 1, 2};
-    float** vals = malloc(sizeof(float*) * 3);
-    float hidden1[] = {0.763774618976614, 0.13436424411240122, 0.8474337369372327};
-    vals[0] = hidden1;
-    float hidden2[] = {0.49543508709194095, 0.2550690257394217, 0.651592972722763, 0.4494910647887381};
-    vals[1] = hidden2;
-
-    struct FFNN* ffnn = alloc(3, layers);
-    ffnn->softMax = 1;
-    setNetwork(ffnn, vals);
-    print(ffnn);
-    float in[] = {1, 0};
-    setInput(ffnn, in);
-    forwardPass(ffnn);
-    float* out = getOutput(ffnn);
-    printf("Sum: %f\n", out[0] + out[1]);
-    printf("Output from forward pass: %f,%f\n", out[0], out[1]);
+    int numImages;
+    int width;
+    int height;
+    float** imgs = readMNISTImages(&numImages, &width, &height);
+    float** labels = readMNISTLabels(&numImages);
     
-    float expected[] = {0, 1};
-    struct NodeGradient** gradient = backwardPass(ffnn, expected);
-    printf("%f\n", gradient[1][0].dBias);
-    printf("%f,%f\n", gradient[2][0].dBias, gradient[2][1].dBias);
-
-    applyGradient(ffnn, gradient, 1);
-    print(ffnn);
+    int inputLayerSize = width * height;
+    int layers[5] = {inputLayerSize, 
+                        32,
+                        10};
+    struct FFNN* ffnn = alloc(3, layers);
+    randomize(ffnn);
+    setCategorical(ffnn);
+    MNISTVisualSGD(ffnn, imgs, labels, numImages, 0.01f, width, height);
     return 0;
 }
 
-// void MNISTVisualStochasticTrain(struct FFNN* ffnn,
-//                     float** inputs, 
-//                     float** outputs, 
-//                     int trainingSetSize, 
-//                     float learningRate,
-//                     int width,
-//                     int height);
 
-// int main() {
-//     int numImages;
-//     int width;
-//     int height;
-//     float** imgs = readMNISTImages(&numImages, &width, &height);
-//     float** labels = readMNISTLabels(&numImages);
+void MNISTVisualSGD(struct FFNN* ffnn,
+                    float** inputs, 
+                    float** outputs, 
+                    int trainingSetSize, 
+                    float learningRate,
+                    int width,
+                    int height) {
+
+    puts("========Training========");
+    int numLines = height + 6;
+    for (int i = 0; i < numLines; ++i) {
+        putchar('\n');
+    }
     
-//     int inputLayerSize = width * height;
-//     int layers[5] = {inputLayerSize, 
-//                         32,
-//                         10};
-//     struct FFNN* ffnn = alloc(3, layers);
-//     enableSoftMax(ffnn, 1);
-//     randomize(ffnn);
-//     // stochasticTrain(ffnn, imgs, labels, numImages, 0.01f);
-//     MNISTVisualStochasticTrain(ffnn, imgs, labels, numImages, 0.001f, width, height);
-//     return 0;
-// }
+    for (int i = 0; i < trainingSetSize; ++i) {
+        sleep(1);
+        setInput(ffnn, inputs[i]);
+        forwardPass(ffnn);
+        struct NodeGradient** gradient = backwardPass(ffnn, outputs[i]);
+        applyGradient(ffnn, gradient, learningRate);
+        free(gradient + 1);
+
+        // =========
+        // Everything after this is a visual addition to the SGD function in FFNN.c
+        // Make sure to have the terminal window tall enough!
 
 
-// void MNISTVisualStochasticTrain(struct FFNN* ffnn,
-//                     float** inputs, 
-//                     float** outputs, 
-//                     int trainingSetSize, 
-//                     float learningRate,
-//                     int width,
-//                     int height) {
+        for (int j = 0; j < numLines; ++j) {
+            // clear screen
+            printf("\033[A\33[2K\r");
+        }
 
-//     puts("========Training========");
-//     int numLines = height + 5;
-//     for (int i = 0; i < numLines; ++i) {
-//         putchar('\n');
-//     }
-    
-//     for (int i = 0; i < trainingSetSize; ++i) {
-//         // sleep(1);
-//         setInput(ffnn, inputs[i]);
-//         forwardPass(ffnn);
-//         struct NodeGradient** gradient = backwardPass(ffnn, outputs[i]);
-//         applyGradient(ffnn, gradient, learningRate);
-//         free(gradient + 1);
+        float* guess = getOutput(ffnn);
+        float cost = quadraticCost(guess, outputs[i], 10);
 
-//         // =========
-//         // Make sure to have the terminal window tall enough
+        char* imgStr = getImgStr(inputs[i], width, height);
+        puts(imgStr);
+        puts("       0123456789");
+        printf("Label: ");
 
+        int goodIndex = -1;
+        for (int j = 0; j < 10; ++j) {
+            if (outputs[i][j]==1) {
+                goodIndex = j;
+            }
+            putchar(shade(outputs[i][j]));
+        }
+        putchar('\n');
+        printf("Probs: ");
+        float max = 0;
+        int guessIndex = -1;
+        for (int j = 0; j < 10; ++j) {
+            if (guess[j] > max) {
+                max = guess[j];
+                guessIndex = j;
+            }
+            putchar(shade(guess[j]));
+        }
+        putchar('\n');
+        printf("Final: ");
+        for (int j = 0; j < 10; ++j) {
+            putchar(shade(j==guessIndex ? 1 : 0));
+        }
 
-//         for (int j = 0; j < numLines; ++j) {
-//             printf("\033[A\33[2K\r");
-//         }
-
-//         float* guess = getOutput(ffnn);
-//         float cost = quadraticCost(guess, outputs[i], 10);
-
-//         char* imgStr = getImgStr(inputs[i], width, height);
-//         puts(imgStr);
-//         puts("       0123456789");
-//         printf("Label: ");
-//         for (int j = 0; j < 10; ++j) {
-//             putchar(shade(outputs[i][j]));
-//         }
-//         putchar('\n');
-//         printf("Guess: ");
-//         for (int j = 0; j < 10; ++j) {
-//             putchar(shade(guess[j]));
-//         }
-//         putchar('\n');
-//         printf("Cost: %.3f\n", cost);
-//     }
-//     putchar('\a');
-// }
+        if (guessIndex == goodIndex) {
+            puts("\033[0;32m GOOD \033[0m");
+        } else {
+            puts("\033[0;31m BAD \033[0m");
+        }
+        
+        putchar('\n');
+        printf("Cost: %.3f\n", cost);
+    }
+    putchar('\a');
+}
