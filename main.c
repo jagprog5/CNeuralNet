059e03128209a6644afedf426f6469022236ede4
John Giorshev <jgiorshev@localhost.localdomain>
2020-03-23 00:11:15 -0400

    Add makefile and demo type argument

--- main.c
+++ main.c
@@ -6,8 +6,6 @@
 #include "FFNN.h"
 #include "MNISTRead.h"
 
-#define DEMOTYPE 1 // set this to 1 or 0
-
 void MNISTVisualSGD(struct FFNN* ffnn,
                     float** inputs, 
                     float** outputs, 
@@ -16,13 +14,16 @@ void MNISTVisualSGD(struct FFNN* ffnn,
                     int width,
                     int height);
 
-int main() {
+int main(int argc, char **argv) {
+    int demoVisual = 1;
+    if (argc > 1) {
+        demoVisual = argv[1][0] - '0';
+    }
+
     int numImages;
     int width;
     int height;
-    if (!DEMOTYPE) {
-        puts("============Training Set============");
-    }
+    puts("============Training Set============");
     float** imgs = readMNISTTrainingImages(&numImages, &width, &height);
     float** labels = readMNISTTrainingLabels(&numImages);
     
@@ -34,21 +35,20 @@ int main() {
     randomize(ffnn);
     setClassifier(ffnn);
 
-    if (DEMOTYPE) {
+    if (demoVisual) {
         MNISTVisualSGD(ffnn, imgs, labels, numImages, 0.01f, width, height);
     } else {
         SGD(ffnn, imgs, labels, numImages, 0.01f);
-        test(ffnn, imgs, labels, numImages);
-        freeSet(imgs, labels, numImages);
-        puts("==============Test Set==============");
-        imgs = readMNISTTestImages(&numImages, &width, &height);
-        labels = readMNISTTestLabels(&numImages);
-        test(ffnn, imgs, labels, numImages);
-
     }
-    freeFFNN(ffnn);
+    test(ffnn, imgs, labels, numImages);
     freeSet(imgs, labels, numImages);
 
+    puts("==============Test Set==============");
+    imgs = readMNISTTestImages(&numImages, &width, &height);
+    labels = readMNISTTestLabels(&numImages);
+    test(ffnn, imgs, labels, numImages);
+    freeSet(imgs, labels, numImages);
+    freeFFNN(ffnn);
     return 0;
 }
 
@@ -59,8 +59,7 @@ void MNISTVisualSGD(struct FFNN* ffnn,
                     float learningRate,
                     int width,
                     int height) {
-
-    puts("========Training========");
+    
     int numLines = height + 5;
     for (int i = 0; i < numLines; ++i) {
         putchar('\n');
@@ -86,9 +85,7 @@ void MNISTVisualSGD(struct FFNN* ffnn,
 
         char* imgStr = getImgStr(inputs[i], width, height);
         puts(imgStr);
-        puts("       0123456789");
-        printf("Label: ");
-
+        printf("       0123456789\nLabel: ");
         int goodIndex = -1;
         for (int j = 0; j < 10; ++j) {
             if (outputs[i][j]) {
@@ -96,14 +93,14 @@ void MNISTVisualSGD(struct FFNN* ffnn,
             }
             putchar(shade(outputs[i][j]));
         }
-        putchar('\n');
-        printf("Probs: ");
+        printf("  %d of %d\nProbs: ", (i + 1), trainingSetSize);
         for (int j = 0; j < 10; ++j) {
             putchar(shade(guess[j]));
         }
-        int guessIndex = maxIndex(guess, 10);
-        printf("  %d of %d\n", (i + 1), trainingSetSize);
-        printf("Final: ");
+        // assuming classifier for MNIST
+        float cost = crossEntropyCost(guess, outputs[i], ffnn->layerSizes[ffnn->numLayers - 1]);
+        printf("  Cost: %.3f\nFinal: ", cost);
+        int guessIndex = maxIndex(guess, 10);        
         for (int j = 0; j < 10; ++j) {
             putchar(shade(j==guessIndex ? guess[j] : 0));
         }
@@ -113,10 +110,6 @@ void MNISTVisualSGD(struct FFNN* ffnn,
         } else {
             puts("\033[0;31m  BAD!\033[0m");
         }
-
-        // assuming classifier for MNIST
-        float cost = crossEntropyCost(guess, outputs[i], ffnn->layerSizes[ffnn->numLayers - 1]);
-        printf("Cost: %.3f\n", cost);
 
         freeNodes(gradient, ffnn->numLayers, ffnn->layerSizes);
     }
