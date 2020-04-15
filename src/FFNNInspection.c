d0d1332823eb494431c6475ead136d37999d78e6
John Giorshev <johngiorshev@gmail.com>
2020-04-15 01:16:27 -0400

    Working ui for training

--- src/FFNNInspection.c
+++ src/FFNNInspection.c
@@ -75,83 +75,30 @@ void populateOutputReceptiveField(float* receptiveField,
     for (int i = 0; i < numInputs; ++i) {
         inputs[i] = 1;
         setInput(ffnn, inputs);
-        for (int l = 1; l < ffnn->numLayers; ++l) {
-            for (int j = 0; j < ffnn->layerSizes[l]; ++j) {
-                if (l == ffnn->numLayers - 1 && j != outputNode) {
-                    continue;
-                }
-                float accum = 0;
-                for (int k = 0; k < ffnn->layerSizes[l - 1]; ++k) {
-                    float input = A(l - 1, k);
-                    float weight = W(l, j, k);
-                    accum += input * weight;
-                }
-                A(l, j) = accum;
-                if (l == ffnn->numLayers - 1) {
-                    receptiveField[i] = accum;
-                }
-            }
-        }
-        inputs[i] = 0;
-    }
-    free(inputs);
-}
-
-float*** allocReceptiveFields(struct FFNN* ffnn) {
-    int numInputs = ffnn->layerSizes[0];
-    float*** fields = malloc(sizeof(*fields) * ffnn->numLayers);
-    for (int l = 0; l < ffnn->numLayers; ++l) {
-        int numNodes = ffnn->layerSizes[l];
-        fields[l] = malloc(sizeof(**fields) * numNodes);
-        for (int j = 0; j < numNodes; ++j) {
-            if (l!=0) {
-                fields[l][j] = malloc(sizeof(***fields) * numInputs);
-            } else {
-                // inputs nodes only set 1 pixel
-                fields[l][j] = calloc(numInputs, sizeof(***fields));
-            }
-        }
-    }
-    return fields;
-}
-
-void populateReceptiveFields(float*** fields, struct FFNN* ffnn) {
-    int numInputs = ffnn->layerSizes[0];
-    float* inputs = calloc(numInputs, sizeof(*inputs));
-    for (int i = 0; i < numInputs; ++i) {
-        inputs[i] = 1;
-        fields[0][i][i] = 1; // input nodes have a receptive field of 1 pixel
-        setInput(ffnn, inputs);
-        
         // modified forwardPass
         // - removes biases
         // - removes activation
         // - logs the weighted inputs
         for (int l = 1; l < ffnn->numLayers; ++l) {
+            int flag = l == ffnn->numLayers - 1;
             for (int j = 0; j < ffnn->layerSizes[l]; ++j) {
+                if (flag) {
+                    j = outputNode;
+                }
                 float accum = 0;
                 for (int k = 0; k < ffnn->layerSizes[l - 1]; ++k) {
                     float input = A(l - 1, k);
                     float weight = W(l, j, k);
                     accum += input * weight;
                 }
-                fields[l][j][i] = accum;
                 A(l, j) = accum;
+                if (flag) {
+                    receptiveField[i] = accum;
+                    break;
+                }
             }
         }
-
         inputs[i] = 0;
     }
-
     free(inputs);
-}
-
-void freeReceptiveFields(float*** fields, struct FFNN* ffnn) {
-    for (int l = 0; l < ffnn->numLayers; ++l) {
-        for (int j = 0; j < ffnn->layerSizes[l]; ++j) {
-            free(fields[l][j]);
-        }
-        free(fields[l]);
-    }
-    free(fields);
 }
